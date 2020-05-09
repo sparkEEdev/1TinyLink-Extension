44a785a4fafaa72f02a1dfe4350c7664d6ac6cf5
sparkEEdev <sparkeeftw@gmail.com>
2020-05-09 15:24:11 +0200

    update popup fake auth flow

--- src/popup.ts
+++ src/popup.ts
@@ -1,8 +1,10 @@
-import { Auth } from 'Interfaces/Auth';
+import { Auth } from 'resources/interfaces/interfaces';
 
 // fake api interaction for the background script
-const loginButton: HTMLButtonElement | null = document.querySelector("#loginBtn");
-const clearBtn: HTMLButtonElement | null = document.querySelector("#clear");
+const loginButton: HTMLButtonElement | null = document.querySelector('#loginBtn');
+const logOutBtn: HTMLButtonElement | null = document.querySelector('#logOutBtn');
+const logInView: HTMLElement | null = document.querySelector('.logInView');
+const loggedInView: HTMLElement | null = document.querySelector('.loggedInView');
 
 const login = (): void => {
 
@@ -16,12 +18,46 @@ const login = (): void => {
     // change popup UI
 }
 
-
 loginButton?.addEventListener('click', () => {
     login();
 })
 
-clearBtn?.addEventListener('click', () => {
+logOutBtn?.addEventListener('click', () => {
     chrome.storage.local.remove(['name', 'token']);
 })
+
+// needs refactoring
+if ( logInView && loggedInView ) {
+    
+    chrome.storage.local.get(['token'], res => {
+        if ( res.token && res.token.length > 0 ) {
+            loggedInView.style.display = 'block';
+            logInView.style.display = 'none';
+        } else {
+            loggedInView.style.display = 'none';
+            logInView.style.display = 'block';
+        }
+    })
+
+    chrome.storage.onChanged.addListener( (changes) => {
+        
+        for (let key in changes) {
+    
+            let change = changes[key];
+    
+            if ( change.newValue && key === 'token' ) {
+                // token added show welcome div
+                loggedInView.style.display = 'block'
+                logInView.style.display = 'none';
+                
+            }
+    
+            if ( change.oldValue && key === 'token' ) {
+                // token removed show login div
+                loggedInView.style.display = 'none'
+                logInView.style.display = 'block';
+            }
+        }
+    })
+}
 // fake api interaction for the background script
