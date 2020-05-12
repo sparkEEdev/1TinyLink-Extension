8ae2560f14ea9aa76276ea4bb7aae9ee5b9aef9a
sparkEEdev <sparkeeftw@gmail.com>
2020-05-12 21:12:54 +0200

    update background script, implemented api for posting links, refactored back to request/response message api for more tab use consistency

--- src/background.ts
+++ src/background.ts
@@ -1,74 +1,95 @@
-import { Link, /* ErrorResponse, */ ResponseChannelPort, RenderContexts } from 'resources/interfaces/interfaces';
-import { isValidURL/* , copyToClipboard */ } from 'resources/helpers';
+import { Link, LinkPayload } from 'resources/interfaces/interfaces';
+import { isValidURL, copyToClipboard, getHeaders } from 'resources/helpers';
 
 const contextTitle: string = 'Make it Tiny!';
 const contextId: string = "1tl";
 const contextsArr: string[] = [ "selection", "link", "image", "editable" ];
-const lifeCycles: string[] = [ "Temporary", "Destructive", "Permanent" ];
 const validatableContexts: string[] = [ "selectionText", "srcUrl", "linkUrl" ];
-const responseChannelPortName: ResponseChannelPort = { name: 'responseChannelPort' };
-let responseChannelPort: chrome.runtime.Port;
+const api = process.env.API_URL;
 
-const makeTiny = ( url: string, lifeCycle: string ): void => {
-    
-    console.log(url, lifeCycle);
+const postForm = async ( data: LinkPayload ): Promise<Response> => {
 
-    // post request 
+    let headers = await getHeaders();
 
-        // copy the fresh tiny link clipboard
-        // copyToClipboard(url); 
-
-        // display response
-        let data: Link = {
-            link: url
-        }
-        // responseChannelPort.postMessage(data);
-        responseChannelPort.postMessage(data);
+    return await fetch(`${api}process`, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data),
+    });
 }
 
-chrome.runtime.onConnect.addListener((port) => {
-    if ( port.name === responseChannelPortName.name ) {
-        responseChannelPort = port;
-    }
-});
+const refreshToken = async (): Promise<void> => {
 
-const handle = (context: chrome.contextMenus.OnClickData, lifeCycle: string ): void => {
-    
+    let headers = await getHeaders();
+
+    await fetch(`${api}refresh`, {
+        method: 'POST',
+        headers: headers,
+    });
+}
+
+const makeTiny = async ( data: LinkPayload, tabId: number ): Promise<void> => {
+
+    let response = await postForm(data);
+
+    if (response.status === 401) {
+
+        refreshToken();
+
+        return chrome.tabs.sendMessage(tabId, { tokenRefresh: true }, (res) => console.log( res ));
+    }
+
+    let json: any = await response.json();
+
+    let tinyLink: Link = { link: json.link };
+
+    copyToClipboard(tinyLink.link);
+
+    chrome.tabs.sendMessage(tabId, tinyLink, () => {});
+}
+
+const handle = ( context: chrome.contextMenus.OnClickData ): string | boolean => {
+
+    // validatableContexts an array of properties delivered by 'context', which potentially hold a valid url
+
     for ( let i in validatableContexts ) {
 
         let url = context[validatableContexts[i]];
 
-        if ( url && isValidURL(url) ) {
-            return makeTiny(url, lifeCycle);
-        }
-        
+        if ( url && isValidURL(url) ) return url;
     }
+
+    return false;
 }
 
-const renderContexts = ({ contextTitle, contextId, contexts, childContexts = [], handler }: RenderContexts ): void => {
+chrome.runtime.onMessage.addListener( ( message, sender, res ) => {
+    // here we listen for incoming message from content script
+    // if the message contains our form data we want to make the post request
+    // and return success message with payload
+    let tab = sender.tab;
 
-    chrome.contextMenus.create({
-        title: contextTitle,
-        id: contextId,
-        contexts: contexts,
-        enabled: false,
-    });
+    if (!(tab && tab.id)) return;
 
-    childContexts.forEach( value => {
+    if ( message.form ) {
+        // make the request
+        makeTiny(message.form, tab.id);
 
-        chrome.contextMenus.create({
-            title: value,
-            parentId: contextId,
-            contexts: contexts,
-            onclick: (context) => {
-                handler(context, value)
-            } 
+        return res({ success: 'form received' });
+    }
+
+    if ( message.hide ) {
+        // send message to hide the element in the tab
+        chrome.tabs.sendMessage(tab.id, message, (res) => {
+            console.log(res);
         });
-    })
-}
+        return res({ success: 'hidden via bg' });
+    }
+
+    res({ failure: 'empty message in background script?'});
+});
 
 chrome.storage.onChanged.addListener( (changes) => {
-        
+    // enable - disable context button depending if we have token or not
     for (let key in changes) {
 
         let change = changes[key];
@@ -88,13 +109,12 @@ chrome.storage.onChanged.addListener( (changes) => {
 })
 
 chrome.runtime.onInstalled.addListener(() => {
-
-    renderContexts({
-        contextTitle: contextTitle,
-        contextId: contextId,
+    // initialize extension context menus and update its useability
+    chrome.contextMenus.create({
+        title: contextTitle,
         contexts: contextsArr,
-        childContexts: lifeCycles,
-        handler: handle
+        id: contextId,
+        enabled: false,
     });
 
     chrome.storage.local.get(['token'], res => {
@@ -107,6 +127,25 @@ chrome.runtime.onInstalled.addListener(() => {
                 enabled: false,
             })
         }
-    })
+    });
+});
 
+chrome.contextMenus.onClicked.addListener((context) => {
+    // here we are listening for our custom button click
+    // after which we validate our url - check const handle();
+    // if we get back a string we send it content script on the frontend
+    // and we display the form on the proper tab
+    let url = handle(context);
+
+    if ( !url ) return;
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+
+        let activeTab = tabs[0];
+        if (!activeTab.id) return;
+
+        chrome.tabs.sendMessage(activeTab.id, { request: 'link', url: url  }, (res) => {
+            console.log(res);
+        });
+    });
 })
