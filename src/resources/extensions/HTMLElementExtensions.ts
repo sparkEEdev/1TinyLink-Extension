66b63601cfa5dc15a7db0cbe05ce1e51e30cb1a5
sparkEEdev <sparkeeftw@gmail.com>
2020-05-21 13:58:36 +0200

    update helpers, remove unnecessary code

--- src/resources/extensions/HTMLElementExtensions.ts
+++ src/resources/extensions/HTMLElementExtensions.ts
@@ -1,20 +1,3 @@
-HTMLElement.prototype.fadeInOut = function (): Animation {
-    // TODO: refactor to keyframes, add params to the prototype method
-    return this.animate([
-        { opacity: '0', zIndex: '-99' },
-        { opacity: '1', zIndex: '9999999' },
-        { opacity: '1' },
-        { opacity: '1' },
-        { opacity: '1' },
-        { opacity: '1' },
-        { opacity: '0', zIndex: '-99' }
-    ], {
-        duration: 2000,
-        iterations: 1,
-        easing: 'ease-in-out',
-    }) 
-}
-
 HTMLElement.prototype.setAttributes = function (attrs: any): void {
 
     for(var key in attrs) {
--- src/resources/interfaces/HTMLElement.d.ts
+++ src/resources/interfaces/HTMLElement.d.ts
@@ -1,5 +1,4 @@
 declare interface HTMLElement {
-    fadeInOut: () => Animation;
     setAttributes: (attrs: any) => void;
     appendChildren: (children: HTMLElement[]) =>  void;
 }
