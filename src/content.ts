17dcb74f4f712cc8e2e2f169aba9203c5794c7a2
sparkEEdev <sparkeeftw@gmail.com>
2020-05-21 14:01:40 +0200

    update content script, refactored if checks

--- src/content.ts
+++ src/content.ts
@@ -1,4 +1,5 @@
 import 'resources/extensions/HTMLElementExtensions';
+import { Link } from "resources/interfaces/interfaces";
 
 const formIFrame: HTMLIFrameElement = document.createElement('iframe');
 formIFrame.setAttributes({ class: 'tinyFrame', id: 'formFrame' });
@@ -10,29 +11,56 @@ responseIFrame.setAttributes({ class: 'tinyFrame', id: 'responseFrame' });
 responseIFrame.src = chrome.runtime.getURL('content/responseIFrame.html');
 document.body.appendChild(responseIFrame);
 
-chrome.runtime.onMessage.addListener( ( message, _sender, res ): void => {
+// TODO: refactor the style changes
+const requestFormHandler = ( _message, _sender, response ): void => {
+    formIFrame.setAttribute('style', 'z-index: 99999; opacity: 1');
+    response({ success: 'request received' });
+}
+
+const linkHandler = ( _message: Link, _sender, response ): void => {
+    responseIFrame.setAttribute('style', 'z-index: 99999; opacity: 1');
+    response({ success: 'link received' });
+}
+
+const errorHandler = ( _message, _sender, response ): void => {
+    responseIFrame.setAttribute('style', 'z-index: 99999; opacity: 1');
+    response({ success: 'error received' });
+}
+
+const hideHandler = ( _message, _sender, response ): void => {
+    formIFrame.setAttribute('style', 'z-index: -99999; opacity: 0');
+    response({ success: 'form hidden' });
+}
+
+const tokenExpiredHandler = ( _message, _sender, response ): void => {
+    responseIFrame.setAttribute('style', 'z-index: 99999; opacity: 1');
+    response({ success: 'logged out' });
+}
+
+
+const hideResponseHandler = ( _message, _sender, response ): void => {
+    responseIFrame.setAttribute('style', 'z-index: -99999; opacity: 0');
+    response({ success: 'copied' });
+}
+
+// eventName: handlerName pairs
+const eventHandlers = {
+    requestForm: requestFormHandler,
+    link: linkHandler,
+    hideForm: hideHandler,
+    error: errorHandler,
+    tokenExpired: tokenExpiredHandler,
+    hideResponse: hideResponseHandler,
+}
+
+chrome.runtime.onMessage.addListener( ( message, sender, res ): void => {
     // here we listen for background messages and animate iframes accordingly
     // data manipulation is happening in the formIFrame.ts && responseIFrame.ts 
-
-    if ( message.link ) {
-        responseIFrame.fadeInOut();
-        return res({ success: 'link received' });
-    }
-
-    if ( message.request && message.request === 'link' ) {
+    for( let event in eventHandlers ) {
         
-        formIFrame.setAttribute('style', 'z-index: 99999; opacity: 1');
-        return res({ success: 'request received' });
-    } 
-
-    if ( message.error ) {
-        responseIFrame.fadeInOut();
-        return res({ success: 'error received' });
-    }
-
-    if ( message.hide && message.hide === 'form' ) {
-        formIFrame.setAttribute('style', 'z-index: -99999; opacity: 0');
-        return res({ success: 'form hidden' });
+        if ( message[event] ) {
+            return eventHandlers[event]( message, sender, res );
+        }
     }
 
     res({ end: 'done' });
