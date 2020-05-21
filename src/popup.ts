0c989f0a1f4639ca2883583a499b02c1cf876ef7
sparkEEdev <sparkeeftw@gmail.com>
2020-05-21 13:59:59 +0200

    update popup html and script, to display the data properly

--- src/popup.ts
+++ src/popup.ts
@@ -2,8 +2,10 @@ import { Auth, AuthPayload } from 'resources/interfaces/interfaces';
 
 const loginButton: HTMLButtonElement | null = document.querySelector('#loginBtn');
 const logOutBtn: HTMLButtonElement | null = document.querySelector('#logOutBtn');
-const logInView: HTMLElement | null = document.querySelector('.logInView');
-const loggedInView: HTMLElement | null = document.querySelector('.loggedInView');
+const logInView: HTMLElement | null = document.querySelector('#logInView');
+const loggedInView: HTMLElement | null = document.querySelector('#loggedInView');
+const name: HTMLSpanElement | null = document.querySelector('#name');
+const subTier: HTMLSpanElement | null = document.querySelector('#tier');
 const api = process.env.API_URL;
 
 const getAuthPayload = (data): AuthPayload => {
@@ -26,7 +28,7 @@ const login = async (): Promise<void> => {
         'Accept': 'application/json'
     });
 
-    let response: Auth = await fetch(`${api}login`, {
+    let response: Auth = await fetch(`${api}/api/v1/login`, {
         method: 'POST',
         headers: headers,
         body: JSON.stringify(payload)
@@ -43,19 +45,23 @@ loginButton?.addEventListener('click', async () => {
 })
 
 logOutBtn?.addEventListener('click', () => {
-    chrome.storage.local.remove(['name', 'token']);
+    chrome.storage.local.remove(['name', 'token', 'subTier', 'token']);
 })
 
-// needs refactoring
-if ( logInView && loggedInView ) {
+// TODO: needs refactoring
+if ( logInView && loggedInView && name && subTier ) {
     
-    chrome.storage.local.get(['token'], res => {
+    chrome.storage.local.get(['token', 'name', 'subTier'], res => {
+        
         if ( res.token && res.token.length > 0 ) {
-            loggedInView.style.display = 'block';
+            loggedInView.style.display = 'grid';
             logInView.style.display = 'none';
+
+            name.innerText = res.name;
+            subTier.innerText = res.subTier;
         } else {
             loggedInView.style.display = 'none';
-            logInView.style.display = 'block';
+            logInView.style.display = 'grid';
         }
     })
 
@@ -66,16 +72,23 @@ if ( logInView && loggedInView ) {
             let change = changes[key];
     
             if ( change.newValue && key === 'token' ) {
-                // token added show welcome div
-                loggedInView.style.display = 'block'
+                // token added show welcome view
+                loggedInView.style.display = 'grid'
                 logInView.style.display = 'none';
-                
+            }
+
+            if ( change.newValue && key === 'name') {
+                name.innerText = change.newValue;
+            }
+
+            if ( change.newValue && key === 'subTier') {
+                subTier.innerText = change.newValue;
             }
     
             if ( change.oldValue && key === 'token' ) {
-                // token removed show login div
+                // token removed show login view
                 loggedInView.style.display = 'none'
-                logInView.style.display = 'block';
+                logInView.style.display = 'grid';
             }
         }
     })
